f97233ee01e662857410f6af2832e8b5aebf8417
MichaelMantovani <michmantovani@gmail.com>
2023-11-17 15:22:44 +0100

    create Regalo class

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -2,6 +2,7 @@ package org.lessons.java.christmas;
 
 public class Main {
 	public static void main(String[] args) {
-
+		Regalo trenino = new Regalo("trenino", "cugino");
+		System.out.println(trenino);
 	}
 }
--- src/org/lessons/java/christmas/Regalo.java
+++ src/org/lessons/java/christmas/Regalo.java
@@ -1,5 +1,33 @@
 package org.lessons.java.christmas;
 
 public class Regalo {
+	private String nome;
+	private String destinatario;
+
+	public Regalo(String nome, String destinatario) {
+		setNome(nome);
+		setDestinatario(destinatario);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDestinatario() {
+		return destinatario;
+	}
+
+	public void setDestinatario(String destinatario) {
+		this.destinatario = destinatario;
+	}
+
+	@Override
+	public String toString() {
+		return "Nome regalo: " + getNome() + "\n" + "Destinatario " + getDestinatario();
+	}
 
 }
